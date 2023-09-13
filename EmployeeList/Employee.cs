d65707e82fa1d6e789eff3fcb7a136440bd8a062
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-13 13:28:01 +0200

    Add Employee

---
+++ EmployeeList/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeList
+{
+    internal class Employee
+    {
+        public string Name { get;  set; }
+        public int Salary { get; }
+
+        public Employee(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -7,22 +7,8 @@ namespace EmployeeList
     {
         static void Main(string[] args)
         {
-            Robot robot = new Robot("Kalle", 2);
-            Robot robot2 = new Robot("Nisse",5);
-
-            Class1 c = new Class1();
-
-            robot.SetFirstName("Nisse");
-            robot.Salary = 10;
-            robot.Salary2 = 10;
-
-            Console.WriteLine(robot.Salary);
-            Console.WriteLine(robot.Salary2);
-
-
-
-            //robot.firstname = "";
-            Console.WriteLine(robot.GetFirstName());
+            Employee emp = new Employee("Kalle", 30000);
+            emp.Name = "Pelle";
         }
     }
 }
