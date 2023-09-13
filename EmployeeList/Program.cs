560635dbc8c17f788b7eac174c9081cf1a1107a9
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-13 11:50:08 +0200

    Add project files.

---
+++ EmployeeList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("Kalle");
+            Robot robot2 = new Robot("Nisse");
+
+            robot.SetFirstName("Nisse");
+
+            
+
+            //robot.firstname = "";
+            //Console.WriteLine(robot.firstname);
+        }
+    }
+}
---
+++ EmployeeList/Robot.cs
@@ -0,0 +1,24 @@
+﻿namespace EmployeeList
+{
+    public class Robot
+    {
+        private  string firstname;
+
+        //public Robot()
+        //{
+
+        //}
+        public Robot(string name)
+        {
+            //Validate
+           // Validate(name);
+            firstname = name;
+        }
+
+        public void SetFirstName(string name)
+        {
+            //Validate
+            firstname = name;
+        }
+    }
+}
