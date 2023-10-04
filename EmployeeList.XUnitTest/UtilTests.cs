b2567a184a163ef037eb011e2880eea56647b5b4
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-10-04 14:21:05 +0200

    With moq

--- EmployeeList.XUnitTest/UtilTests.cs
+++ EmployeeList.XUnitTest/UtilTests.cs
@@ -1,18 +1,31 @@
 using EmployeeList.Helpers;
 using EmployeeList.UI;
+using Moq;
 
 namespace EmployeeList.XUnitTest
 {
     public class UtilTests
     {
         [Fact]
-        public void Test1()
+        public void AskForString_ShouldReturnString()
         {
             const string expected = "some input";
             var mockUI = new MockUI();
             mockUI.SetInput = expected;
 
             var actual = Util.AskForString("", mockUI);
+
+            Assert.Equal(expected, actual); 
+        }  
+        
+        [Fact]
+        public void AskForString_ShouldReturnString_WithMOQ()
+        {
+            const string expected = "some input";
+            var mockUI = new Mock<IUI>();
+            mockUI.Setup(u => u.GetInput()).Returns(expected);
+
+            var actual = Util.AskForString("", mockUI.Object);
 
             Assert.Equal(expected, actual); 
         }
